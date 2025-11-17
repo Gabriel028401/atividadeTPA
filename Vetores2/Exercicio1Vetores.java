e379a52f742a3a717539d2fb5d0f808591c25b71
Gabriel028401 <feranandes40@gmail.com>
2025-11-16 22:07:21 -0300

    Create Exercicio1Vetores.java

---
+++ Vetores2/Exercicio1Vetores.java
@@ -0,0 +1,49 @@
+package vetores;
+
+import java.util.Scanner;
+
+public class Exercicio1Vetores {
+	
+	    public static void main(String[] args) {
+
+	    
+	        Scanner ler = new Scanner(System.in);
+
+	     
+	        int[] a = new int[20];
+
+
+        
+	        int[] b = new int[20];
+	        
+	        int j = 0; //indice b
+
+	        
+	        System.out.println("Por favor, digite 20 números inteiros:");
+	        for (int i = 0; i < 20; i++) {
+	            System.out.print("a[" + (i+1) + "]: ");
+	            a[i] = ler.nextInt();
+	        }
+
+	        for (int i = 0; i < 20; i++) {
+	            if (a[i] % 2 == 0) {
+	                b[j] = a[i];
+	                j++; 
+	            }
+	        }
+	        for (int i = 0; i < 20; i++) {
+	            if (a[i] % 2 != 0) {
+	                b[j] = a[i];
+	                j++; 
+	            } // Se o resulto da divisão por 2 for diferente de 0, o número é ÍMPAR.
+	        }
+
+	        System.out.println("\n--- Resultado do vetor B ---");
+	        for (int i = 0; i < 20; i++) {
+	            System.out.print(b[i] + " ");
+	        }
+	        System.out.println();
+	  
+	    }
+	}
+	
